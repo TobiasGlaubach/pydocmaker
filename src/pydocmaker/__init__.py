e793b7b3102649fe1b04372b91427acdd376c234
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2025-02-14 13:16:49 +0100

    adjusted inits

--- src/pydocmaker/__init__.py
+++ src/pydocmaker/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '1.4.3'
 
-from pydocmaker.core import DocBuilder, construct, constr, buildingblocks, print_to_pdf
+from pydocmaker.core import DocBuilder, construct, constr, buildingblocks, print_to_pdf, get_latex_compiler, set_latex_compiler, make_pdf_from_tex
 from pydocmaker.util import upload_report_to_redmine
 
 from pydocmaker.core import DocBuilder as Doc
@@ -8,3 +8,5 @@ from pydocmaker.core import DocBuilder as Doc
 def get_schema():
     return {k: getattr(constr, k)() for k in buildingblocks}
         
+def get_example():
+    return Doc.get_example()
