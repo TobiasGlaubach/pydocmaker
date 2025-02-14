2485e5c6c1bc539f283bc63ffe1139b4c0e70806
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2025-02-14 13:19:19 +0100

    incremented version before merge

--- src/pydocmaker/__init__.py
+++ src/pydocmaker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.4.3'
+__version__ = '2.0.0'
 
 from pydocmaker.core import DocBuilder, construct, constr, buildingblocks, print_to_pdf, get_latex_compiler, set_latex_compiler, make_pdf_from_tex
 from pydocmaker.util import upload_report_to_redmine
