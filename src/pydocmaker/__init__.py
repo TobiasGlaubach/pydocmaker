e5f829634ddd6251bd29110fbdad1c1049135f0f
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2026-01-26 19:05:45 +0100

    Updated python project link to readthedocs

--- src/pydocmaker/__init__.py
+++ src/pydocmaker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.5.1'
+__version__ = '2.5.2'
 
 from pydocmaker.core import Doc, construct, constr, buildingblocks, print_to_pdf, get_latex_compiler, set_latex_compiler, make_pdf_from_tex, show_pdf
 from pydocmaker.util import upload_report_to_redmine, bcolors, txtcolor, colors_dc
