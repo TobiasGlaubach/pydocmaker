bf33e2c5e2422fec3a7de06131721adaa5a213cf
Tobias Glaubach <tobias.glaubach@gmail.com>
2026-05-15 00:08:31 +0200

    v2.6.2: added lost testcases again and added a pypi publishing workflow.

--- src/pydocmaker/__init__.py
+++ src/pydocmaker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.6.1'
+__version__ = '2.6.2'
 
 from pydocmaker.core import Doc, construct, constr, buildingblocks, print_to_pdf, make_pdf_from_tex, show_pdf, is_notebook
 from pydocmaker.util import upload_report_to_redmine, bcolors, txtcolor, colors_dc, _raise_missing
